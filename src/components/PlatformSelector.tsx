c533aeb45f6c08912bb7a67b9d29679f55607ff7
JoshThanki <joshthanki@gmail.com>
2023-08-26 16:19:02 +0100

    init

---
+++ src/components/PlatformSelector.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+import {
+  Button,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
+  Spinner,
+} from "@chakra-ui/react";
+import { BsChevronBarDown, BsChevronDown } from "react-icons/bs";
+import { usePlatforms } from "../hooks/usePlatforms";
+import { Platform } from "../data/types";
+
+interface Props {
+  onSelectPlatform: (platform: Platform) => void;
+  selectedPlatform: Platform | null;
+}
+
+const PlatformSelector = ({ onSelectPlatform, selectedPlatform }: Props) => {
+  const { data, error, isLoading } = usePlatforms();
+
+  if (error) return null;
+
+  return (
+    <Menu>
+      <MenuButton as={Button} rightIcon={<BsChevronDown />}>
+        {selectedPlatform?.name || "Platforms"}
+      </MenuButton>
+      <MenuList>
+        <>
+          {isLoading && <Spinner />}
+
+          {data?.results.map((platform) => (
+            <MenuItem
+              onClick={() => onSelectPlatform(platform)}
+              key={platform.id}
+            >
+              {platform.name}
+            </MenuItem>
+          ))}
+        </>
+      </MenuList>
+    </Menu>
+  );
+};
+
+export default PlatformSelector;
